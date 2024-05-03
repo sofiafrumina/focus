369acc0545b0effa93686c5f67090263ebc5f91a
sofiafrumina <sofiafrumina@mail.ru>
2024-05-03 20:45:15 +0400

    update base files

--- popup.js
+++ popup.js
@@ -1,19 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const applyChangesButton = document.getElementById('applyChanges');
-  
-    applyChangesButton.addEventListener('click', function() {
-      const backgroundColor = document.getElementById('backgroundColor').value;
-      const textColor = document.getElementById('textColor').value;
-      const fontSize = document.getElementById('fontSize').value + 'px';
-      const lineHeight = document.getElementById('lineHeight').value;
-      const readingSpeed = document.getElementById('readingSpeed').value;
-  
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, {
-          action: 'applyChanges',
-          styles: { backgroundColor, textColor, fontSize, lineHeight, readingSpeed }
-        });
-      });
-    });
-  });
-  
+const focusBtn = document.getElementById("focusBtn");
+grabBtn.addEventListener("click",() => {    
+    alert("CLICKED");
+})
